f1083e7082a45a8592cd80ea719db3289638f447
Petterson Guilherme <petterson.guilhermegg@gmail.com>
2026-01-30 22:01:40 -0300

    config: Adding pylint to project

---
+++ ex_pylint.py
@@ -0,0 +1,7 @@
+
+
+print("ola mundo")
+
+#snake_case -> minha_variavel - usado em variaveis
+#camelCase -> minhaVariavel - considerado errado em python
+#PascalCase -> MinhaClasse - usado em classes
