6d66854f5556d2894d57649bcd8c55c7ba1be803
Petterson Guilherme <petterson.guilhermegg@gmail.com>
2026-02-06 16:41:24 -0300

    feat: Implementing people_repository

--- src/models/sqlite/repositories/repositories_test.py
+++ src/models/sqlite/repositories/repositories_test.py
@@ -1,8 +1,9 @@
 import pytest
 from src.models.sqlite.settings.connection import db_connection_handler
+from .people_repository import PeopleRepository
 from .pets_repository import PetsRepository
 
-# db_connection_handler.connect_to_db()
+#db_connection_handler.connect_to_db()
 
 @pytest.mark.skip(reason="Interaction with db")
 def test_list_pets():
@@ -16,3 +17,22 @@ def test_delte_pet():
     name = "belinha"
     repo = PetsRepository(db_connection_handler)
     repo.delete_pets(name)
+
+@pytest.mark.skip(reason="Interaction with db")
+def test_insert_person():
+    fist_name = "test name"
+    last_name = "last test"
+    age = 79
+    pet_id = 2
+
+    repo = PeopleRepository(db_connection_handler)
+    repo.insert_person(fist_name, last_name, age, pet_id)
+
+@pytest.mark.skip(reason="Interaction with db")
+def test_get_person():
+    person_id = 1
+
+    repo = PeopleRepository(db_connection_handler)
+    response = repo.get_person(person_id)
+    print()
+    print(response)
